4e1b86f9249337591cb6a12b502f509b986f21cc
unknown <1527848328@qq.com>
2019-11-10 11:02:35 +0800

    created a new ketang1

---
+++ ketang1/Book.java
@@ -0,0 +1,13 @@
+package com.goolge.ketang.ketang1;
+
+public class Book {
+    protected int pages = 300;
+
+    public int getPage() {
+        return pages;
+    }
+
+    public void setPage(int page) {
+        this.pages = pages;
+    }
+}
